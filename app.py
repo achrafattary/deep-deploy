ef1c80c312e87c52ef211350d3705b589ecf8e10
achrafattary <67131200+achrafattary@users.noreply.github.com>
2022-05-31 15:54:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
-from __future__ import division, print_function
 import os
-
-
-# Keras
-
-
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 # Flask utils
 from flask import Flask, request, render_template
-from werkzeug.utils import secure_filename
+
 
 # Define a flask app
 app = Flask(__name__)
-
-# Model saved with Keras model.save()
-
-
-# Load your trained model
-
-
-
-
-
-
-
 @app.route('/', methods=['GET'])
 def index():
     # Main page

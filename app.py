562882a216fb078e87de9e9836c5446695f49920
achrafattary <67131200+achrafattary@users.noreply.github.com>
2022-06-01 10:49:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 
 # Flask utils
 from flask import Flask, request, render_template
-from tensorflow-cpu.keras.models import load_model
 
 # Define a flask app
 app = Flask(__name__)
@@ -10,6 +9,17 @@ def index():
     # Main page
     return render_template('index.html')
 
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+    return None
 
 
 
